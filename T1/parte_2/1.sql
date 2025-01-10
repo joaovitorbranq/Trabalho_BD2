c8052d51ea79cd30b8a1087198f677e1fb0f289c
Yakson Chan <yaksonchan@id.uff.br>
2025-01-10 15:25:58 -0300

    Adicionando regras semânticas que usaremos na Q1 da P2

--- T1/parte_2/1.sql
+++ T1/parte_2/1.sql
@@ -1,3 +1,6 @@
 -- Criar regras semânticas, que são regras que não podem ser garantidas pela estrutura
 -- do modelo relacional, usando o esquema exemplo fornecido. As regras criadas também
 -- devem ser descritas textualmente no trabalho a ser entregue.
+
+-- Regra 1: Na tabela Invoice a coluna total deve ser exatamente a soma de (unit_price x quantity) da tabela de InvoiceLine pertencentes a Invoice
+-- Regra 2: Validar que o total de uma Invoice não seja negativa
