b1008ea8d281702db81dbb8924fcdc21014fee07
Yakson Chan <yaksonchan@id.uff.br>
2025-01-10 15:46:34 -0300

    Corrigindo problema de referência a constraints na Q2 da P1

--- T1/parte_1/2.sql
+++ T1/parte_1/2.sql
@@ -1,14 +1,23 @@
 -- Criar usando a linguagem de programação do SGBD escolhido um procedimento que
 -- remova todos os índices de uma tabela informada como parâmetro.
 
--- Verificar sobre PKEYs e Cascade se são necessários, ou é isso mesmo que a questão quer
-
 CREATE OR REPLACE PROCEDURE drop_all_indexes(table_name TEXT)
 LANGUAGE plpgsql
 AS $$
 DECLARE
     index_name TEXT;
+    constraint_record RECORD;
 BEGIN
+    -- Remover restrições da tabela
+    FOR constraint_record IN 
+        SELECT conname
+        FROM pg_constraint
+        WHERE conrelid = (SELECT oid FROM pg_class WHERE relname = table_name)
+    LOOP
+        EXECUTE 'ALTER TABLE ' || table_name || ' DROP CONSTRAINT ' || constraint_record.conname || ' CASCADE';
+    END LOOP;
+
+    -- Remover índices associados à tabela
     FOR index_name IN
         SELECT indexname
         FROM pg_indexes
